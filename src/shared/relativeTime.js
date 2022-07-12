5dde2583327e882bf6d01e701d6dd080410db8be
itsme.fui <48958743+tranvangiahuy1999@users.noreply.github.com>
2022-07-12 15:14:02 +0700

    Update relativeTime.js

--- src/shared/relativeTime.js
+++ src/shared/relativeTime.js
@@ -1,24 +1,26 @@
 export const relativeTime = {
   relativeTime(seconds) {
+    const oldTimestamp = seconds - 86400;
+    const difference = seconds - oldTimestamp;
     let output = ``;
-    if (seconds < 60) {
+    if (difference < 60) {
       // Less than a minute has passed:
-      output = `${seconds} seconds ago`;
-    } else if (seconds < 3600) {
+      output = `${difference} seconds ago`;
+    } else if (difference < 3600) {
       // Less than an hour has passed:
-      output = `${Math.floor(seconds / 60)} minutes ago`;
-    } else if (seconds < 86400) {
+      output = `${Math.floor(difference / 60)} minutes ago`;
+    } else if (difference < 86400) {
       // Less than a day has passed:
-      output = `${Math.floor(seconds / 3600)} hours ago`;
-    } else if (seconds < 2620800) {
+      output = `${Math.floor(difference / 3600)} hours ago`;
+    } else if (difference < 2620800) {
       // Less than a month has passed:
-      output = `${Math.floor(seconds / 86400)} days ago`;
-    } else if (seconds < 31449600) {
+      output = `${Math.floor(difference / 86400)} days ago`;
+    } else if (difference < 31449600) {
       // Less than a year has passed:
-      output = `${Math.floor(seconds / 2620800)} months ago`;
+      output = `${Math.floor(difference / 2620800)} months ago`;
     } else {
       // More than a year has passed:
-      output = `${Math.floor(seconds / 31449600)} years ago`;
+      output = `${Math.floor(difference / 31449600)} years ago`;
     }
     return output;
   },
